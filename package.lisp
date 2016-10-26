343781c22f4d9cf737e45e8c3a9d91be37918b49
Nick Matvyeyev <mnasoft@gmail.com>
2016-10-26 21:00:21 +0300

    2016-10-26 21:0:20 mnasoft-00

--- package.lisp
+++ package.lisp
@@ -7,7 +7,9 @@
    circle-area-by-radius
    circle-diameter-by-area
    equivalent-area-group-holes
-   parts))
+   parts
+   axial-swirler
+   ))
 
 ;;;;(declaim (optimize (space 0) (compilation-speed 0)  (speed 0) (safety 3) (debug 3)))
 
